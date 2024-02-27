ae5afa341931cf62755c4e5319dc594f15ebab47
AlexHaleson <161532772+AlexHaleson@users.noreply.github.com>
2024-02-28 00:23:34 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,61 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+
+if (ScrollTrigger.isTouch !==1){
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true,
+})
+
+    gsap.fromTo('.hero-section',{opacity: 1},{
+        opacity: 0,
+        scrollTrigger:{
+            trigger: '.hero-section',
+            start: 'center',
+            end: '820',
+            scrub: true,
+        }
+
+    } )
+
+    let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+    itemsL.forEach( item => {
+        gsap.fromTo(item, {x: -100,   opacity: .2 },{
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            scrub: true
+        }
+   })
+
+    })
+    let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+    itemsR.forEach( item => {
+        gsap.fromTo(item, {x: 100,   opacity: .2 },{
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            scrub: true
+        }
+   })
+
+    })
+    let itemsF = gsap.utils.toArray('.social .footer-img')
+
+    itemsF.forEach( item => {
+        gsap.fromTo(item, {y: -10,   opacity: .5 },{
+        opacity: 1, x: 0,
+        scrollTrigger: {
+            trigger: item,
+            scrub: true
+        }
+   })
+   })
+}
+
+
+
